68b3152f1325c9225ae9adf7ced8a3a88ec125ab
mdsharifremon <sharifuddinremon@gmail.com>
2021-06-09 21:26:32 +0600

    ajax

---
+++ ajax/ajaxload.php
@@ -0,0 +1,17 @@
+<?php 
+$conn = mysqli_connect('localhost', 'root', '', 'cms_form') or die(mysqli_connect_error());
+$sql = "SELECT * FROM user";
+$result = mysqli_query($conn,$sql) or die(mysqli_error($conn));
+$output = '';
+
+if(mysqli_num_rows($result) > 0){
+
+    $output = "<table class='inner'>
+               <tr><th>if</th><th>First Name</th><th>Last Name</th> <th>Age</th></tr>";
+        while($row=mysqli_fetch_assoc($result)){
+            $output .= "<tr><td>$row[id]</td><td>$row[fname]</td><td>$row[lname]</td><td>$row[age]</td></tr>";
+        }
+    $output .= "</table>";
+    mysqli_close($conn);
+    echo $output;
+}
--- email/new.php
+++ email/new.php
@@ -1,11 +1,17 @@
-
- <?php 
-                        $email = 'sharifwds@gmail.com';
-                        $subject = 'This is a test email';
-                        $body = "Hi, This is test email send by PHP Script";
-                        $headers = "From: sharifuddinremon@gmail.com";
-                        if (mail($email, $subject, $body, $headers)) {
-                            echo "Email successfully sent to $email...";
-                        } else {
-                            echo "Email sending failed...";
-                        };
+<?php 
+$to = 'sharifwds@gmail.com';
+$subject = 'Test email from php';
+$body = "<DOCTYPE html><html><head><meta charset='UTF-8'></head><body><h1 style='color:red;'>This is a simple email</h1><p style='color:blue;'>This is paragraph</p><br><a href='https://sharifwds.me'>This is a link</a></body></html>";
+$header = [
+             "MIME-Version : 1.0",
+             "Content-Type : html/text; Charset=utf8",
+             "From : sharif@sharifwds.me",
+             "Cc: sharif4career@gmail.com",
+];
+$header = implode("\r\n", $header);
+echo $header;
+if(mail($to, $subject, $body, $header)){
+    echo "Mail has been send to $to";
+}else{
+    echo "Email sending failed";
+};
