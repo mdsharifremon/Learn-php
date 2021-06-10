f7e7f744e60a71672bdaac56c2a93815a94a77d1
mdsharifremon <sharifuddinremon@gmail.com>
2021-06-10 21:30:57 +0600

    arrange folders

--- index.php
+++ index.php
@@ -6,8 +6,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="favicon1.ico" type="image/x-icon">
-    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="src/favicon1.ico" type="image/x-icon">
+    <link rel="stylesheet" href="src/style.cssstyle.css">
     <style>
       input{
         padding:5px 10px;
@@ -28,6 +28,5 @@
  
  ?>  
 </div>
-<script src="learn.js"></script>
 </body>
 </html>
