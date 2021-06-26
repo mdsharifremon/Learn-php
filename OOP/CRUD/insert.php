7a574fe2031b9da0970ce33b50d37cfa8957e269
mdsharifremon <sharifuddinremon@gmail.com>
2021-06-26 09:24:52 +0600

    CRUD - INSERT

--- OOP/CRUD/insert.php
+++ OOP/CRUD/insert.php
@@ -2,6 +2,7 @@
 
 require_once 'database.php';
 $conn = new Database();
+// Insert Data To Database
 $conn->insert('user', ['fname' => 'sharif', 'lname' => 'uddin', 'age' => 27, 'city' => 'Ctg', 'dob' => '31/12/1994']);
 print_r($conn->get_result());
 ?>
