29905a6caf6180dec7eb226c706bf4da93d97b62
mdsharifremon <sharifuddinremon@gmail.com>
2021-06-26 09:22:39 +0600

    load data CRUD

--- OOP/CRUD/load-data.php
+++ OOP/CRUD/load-data.php
@@ -3,7 +3,7 @@
 require_once 'database.php';
 
 $conn = new Database();
-// $conn->sql("SELECT * FROM USER WHERE age > 30");
+// $conn->sql("SELECT * FROM USER WHERE age > 30"); // By sql command
 $conn->select('user', 'fname, lname', 'age > 30', 'age DESC', 5);
 echo '<pre>';
 print_r($conn->get_result());
