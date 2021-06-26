e27fdbcf278071441a212591280424cded207bbb
mdsharifremon <sharifuddinremon@gmail.com>
2021-06-26 09:25:32 +0600

    CRUD - Update

--- OOP/CRUD/update.php
+++ OOP/CRUD/update.php
@@ -2,6 +2,7 @@
 
 require_once 'database.php';
 $conn = new Database();
+// CRUD - UPDATE
 $conn->update('user', ['fname' => 'Karim', 'lname' => 'miah', 'age' => 27, 'city' => 'Ctg', 'dob' => '31/12/1994'], 'id=26');
 print_r($conn->get_result());
 ?>
