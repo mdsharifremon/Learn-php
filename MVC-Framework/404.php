4a3f4de1d2d924f362301993f86172282c5e5565
mdsharifremon <sharifuddinremon@gmail.com>
2021-06-29 20:05:41 +0600

    htaccess

---
+++ MVC-Framework/404.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MVC FRAMEWORK</title>
+</head>
+<body>
+    <h1>Ops! Page Not Found</h1>
+    <p><a href='public/index.php'>Back to Home Page</a></p>
+</body>
+</html>
